a1880c9493e5a64510100d951472ff9b9e63dc42
DB1132 <baldhadeep5@gmail.com>
2026-05-15 16:52:09 +0530

    Instal React-router-dom packge

--- Day-4/src/App.jsx
+++ Day-4/src/App.jsx
@@ -3,13 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Api1 from './Api1'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<Api1/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
